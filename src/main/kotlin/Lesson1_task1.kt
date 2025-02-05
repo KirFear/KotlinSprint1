6dd9e144384c4fd4e26d6bb0446c8ee35d8f293a
KirFear <sukir@list.ru>
2025-02-05 21:24:09 +0300

    KS1-1-1 variables

--- src/main/kotlin/Lesson1_task1.kt
+++ src/main/kotlin/Lesson1_task1.kt
@@ -3,6 +3,6 @@ package org.example
 fun main() {
     val ordersCount = 75
     val gratitudeText = "Спасибо за покупку! Без вас мы не добились бы успеха!"
-println("Ваш заказ №$ordersCount")
-println(gratitudeText)
+    println("Ваш заказ №$ordersCount")
+    println(gratitudeText)
 }
